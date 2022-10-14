737adaff51b7c09102c1d9a235ebe5104eeb7449
Deepin Developer <deepin-dev@deepin.org>
2022-10-14 11:11:53 +0800

    feat: Init commit
    init node-magic-string
    
    Log:

---
+++ rollup.config.js
@@ -0,0 +1,50 @@
+import buble from 'rollup-plugin-buble';
+import resolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
+
+const plugins = [
+	buble({ exclude: 'node_modules/**' }),
+	resolve(),
+	replace({ DEBUG: false })
+];
+
+export default [
+	/* esm */
+	{
+		input: 'src/index.js',
+		external: ['sourcemap-codec'],
+		plugins,
+		output: {
+			file: 'dist/magic-string.es.js',
+			format: 'es',
+			exports: 'named',
+			sourcemap: true
+		}
+	},
+
+	/* cjs */
+	{
+		input: 'src/index-legacy.js',
+		external: ['sourcemap-codec'],
+		plugins,
+		output: {
+			file: 'dist/magic-string.cjs.js',
+			format: 'cjs',
+			exports: 'default',
+			sourcemap: true
+		}
+	},
+
+	/* umd */
+	{
+		input: 'src/index-legacy.js',
+		plugins,
+		output: {
+			file: 'dist/magic-string.umd.js',
+			format: 'umd',
+			exports: 'default',
+			name: 'MagicString',
+			sourcemap: true
+		}
+	}
+];
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import MagicString from './MagicString.js';
+
+export default MagicString;
+export { default as Bundle } from './Bundle.js';
+export { default as SourceMap } from './SourceMap.js';
